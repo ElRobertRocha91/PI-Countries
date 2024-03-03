91af4721071fe713cb35a4edabb9c66ae9f102be
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2024-03-03 11:30:34 -0300

    update

--- PI-Countries-Roberto/api/src/controllers/countriesController.js
+++ PI-Countries-Roberto/api/src/controllers/countriesController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { Country } = require("../db");
 
 const getApiInfo = async () => {
 
@@ -16,10 +17,57 @@ const getApiInfo = async () => {
             population: element.population
         }
     });
-    console.log("Esto me devuelve la API: ",apiInfo);
+    console.log("Solicitud de la API exitosa");
     // apiInfo ==> array of object: [{},{},{},...];
     return apiInfo;
 
 }
 
-module.exports = { getApiInfo };
+// Save information of API in DATABASE
+const saveInfo = async () => {
+
+    const arrCountries = await getApiInfo();
+    // I create database and load it wich the information
+    await Country.bulkCreate(arrCountries);
+    const data = await Country.findAll();
+    // Validatión
+    if(data.length){
+        console.log("Carga de base de datos exitosa");
+        return data;
+    }
+}
+
+
+// Get information into DATABASE
+const getInfoData = async () => {
+    const countries = await Country.findAll();
+    
+    // If database does not exist, create it.
+    // But if the database already exist, I just fetch the data.
+    if(!countries.length){
+        const infoData = await saveInfo();
+        const data = infoData.map(el => {
+            return {
+                id: el.id,
+                image: el.flag,
+                name: el.name,
+                continent: el.continent
+            }
+        });
+        console.log("Base de datos vacia, recien cargada");
+        return data;
+    }else{   
+        const arrData = countries.map(el => {
+            return {
+                id: el.id,
+                image: el.flag,
+                name: el.name,
+                continent: el.continent
+            }
+        });
+        console.log("Base de datos envio la info, ya esta cargada");
+        return arrData;
+    }
+}
+
+module.exports = { getApiInfo, saveInfo, getInfoData };
--- PI-Countries-Roberto/api/src/handlers/countriesHandler.js
+++ PI-Countries-Roberto/api/src/handlers/countriesHandler.js
@@ -1,8 +1,8 @@
-const { getApiInfo } = require("../controllers/countriesController");
+const { getApiInfo, saveInfo, getInfoData } = require("../controllers/countriesController");
 
 const getCountries = async (req, res) => {
     try {
-        const allCountries = await getApiInfo();
+        const allCountries = await getInfoData();
         res.status(200).json(allCountries);
     } catch (error) {
         console.log(error);
