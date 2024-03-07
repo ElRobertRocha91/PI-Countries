a28a0716b6c3c1037a2b712d211d181a6525bfee
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2024-03-07 17:45:57 -0300

    Update handler country

--- PI-Countries-Roberto/api/src/handlers/countriesHandler.js
+++ PI-Countries-Roberto/api/src/handlers/countriesHandler.js
@@ -1,11 +1,26 @@
-const { getApiInfo, saveInfo, getInfoData } = require("../controllers/countriesController");
+const { getInfoData } = require("../controllers/countriesController");
+const { getCountryByName } = require("../controllers/countryByNameController");
 
 const getCountries = async (req, res) => {
     try {
-        const allCountries = await getInfoData();
-        res.status(200).json(allCountries);
+        const { name } = req.query;
+
+        if(!name){
+            const allCountries = await getInfoData();
+            res.status(200).json(allCountries);
+        }else{
+            const countryByName = await getCountryByName(name);
+            // console.log("=====Búsqueda Exitosa por name======");
+            // Validation
+            if(!countryByName){
+                throw new Error(`No existe el país con el nombre: ${name}, por favor ingrese otro nombre!`);
+            }else{
+                res.status(200).json(countryByName);
+            }
+        }
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
+        res.status(404).json(error.message);
     }
 }
 
